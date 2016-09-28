cf8d7d72c3dbbd6ef3fbd69aae846559d47c1a9a
Oscar Fraxedas <oscar.fraxedas.tormo@oracle.com>
2016-09-28 07:43:18 -0400

    And there is an app

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name medellauiApp
+ * @description
+ * # medellauiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('medellauiApp', []);
